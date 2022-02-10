bc544d3bcede6d148f6647a03d1b5ab620d249a9
Kabao Thao <KabaoKThao@gillettechildrens.com>
2022-02-10 09:58:16 -0600

    Add project files.

---
+++ CSharpVariables/Program.cs
@@ -0,0 +1,35 @@
+﻿using System; //all these stuffs are configuring the files. we can specify these things we can use. 
+
+namespace StaticClassAttributes //same name as the project we are using. we are in the project.
+                                //these two curly brackets is where we start our coding
+{
+    public static class Program //class is like a container that we can put our code. 
+    {
+        static void Main(string[] args) //this is sort of like another container we can put our code. This Main method any code we put inside of these curly brackets it will execute our code. 
+        {
+
+            //Console.WriteLine("Hello World");//writes a line on to the screen
+            //Console.ReadLine(); //read the line and then close the screen
+
+            Console.WriteLine("   /|"); //once its done with the first instruction.. it will execute in order. the order of our program matters. 
+            Console.WriteLine("  / |");
+            Console.WriteLine(" /  |");
+            Console.WriteLine("/___|");
+
+            Console.ReadLine();
+
+
+        }
+
+
+
+
+    }
+
+
+}
+
+/*
+ Just a set of instructions that the computer is going to follow the order of those instructions.
+
+ */
